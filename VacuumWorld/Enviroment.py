621828751ce8cc685f16f23a163eda1eb1eaf779
Brena <brenarodrigues@alu.ufc.br>
2022-09-29 11:06:56 -0300

    atv1 finalizada

--- VacuumWorld/Enviroment.py
+++ VacuumWorld/Enviroment.py
@@ -5,39 +5,39 @@ class Enviroment:
     self.__isDirtyB = isDirtyB
     self.__agentLocation = agentLocation
     
-    def getIsDirtyA(self):
-      return self.__isDirtyA
+  def getIsDirtyA(self):
+    return self.__isDirtyA
+  
+  def setIsDirtyA(self, isDirtyA):
+    self.__isDirtyA = isDirtyA
+  
+  def getIsDirtyB(self):
+    return self.__isDirtyB
+  
+  def setIsDirtyB(self, isDirtyB):
+    self.__isDirtyB = isDirtyB
+  
+  def getAgentLocation(self):
+    return self.__agentLocation
+  
+  def setAgentLocation(self, agentLocation):
+    self.__agentLocation = agentLocation
     
-    def setIsDirtyA(self, isDirtyA):
-      self.__isDirtyA = isDirtyA
+  def update(self, action):
     
-    def getIsDirtyB(self):
-      return self.__isDirtyB
-    
-    def setIsDirtyB(self, isDirtyB):
-      self.__isDirtyB = isDirtyB
-    
-    def getAgentLocation(self):
-      return self.__agentLocation
-    
-    def setAgentLocation(self, agentLocation):
-      self.__agentLocation = agentLocation
-      
-    def update(self, action):
-      
-      if action=='aspirar':
-        if self.__agentLocation and self.__isDirtyA: #limpa A
-          self.__isDirtyA=False
-        elif self.__agentLocation==False and self.__isDirtyB:
-          self.__isDirtyB=False
-          
-      elif action=='direita':
-        if self.__agentLocation:      #se estiver na sala A
-          setAgentLocation(False)   #vai pra sala B
-          
-      elif action=='esquerda':
-        if not self.__agentLocation: #se não estiver na sala A
-          setAgentLocation(True)   #vai pra sala A
+    if action=='aspirar':
+      if self.__agentLocation and self.__isDirtyA: #limpa A
+        self.__isDirtyA=False
+      elif self.__agentLocation==False and self.__isDirtyB:
+        self.__isDirtyB=False
         
+    elif action=='direita':
+      if self.__agentLocation:      #se estiver na sala A
+        self.setAgentLocation(False)   #vai pra sala B
+        
+    elif action=='esquerda':
+      if not self.__agentLocation: #se não estiver na sala A
+        self.setAgentLocation(True)   #vai pra sala A
+      
         
         
--- VacuumWorld/VacuumWorld.py
+++ VacuumWorld/VacuumWorld.py
@@ -2,14 +2,28 @@ from Enviroment import Enviroment
 from Agent import Agent
 
 if __name__ == '__main__':
-  
-  env = Enviroment(True, True, True)
+  entrada = input('Digite a configuração do ambiente: isDirtyA, isDirtyB, agentLocation ').split()
+  entrada = [True if x=='True' else False for x in entrada ]
+  env = Enviroment(entrada[0],entrada[1],entrada[2])
   steps = int(input('Digite o número de ações: '))
   
   agent = Agent()
   
   for i in range(steps):
     p = agent.perceives(env)
-    print(p)
+    sala = 'A' if p.getLocation() else 'B'
+    estado = 'suja' if p.getIsDirty() else 'limpa'
+
+    print('Agente está na sala ' + sala+ ' que está '+estado)
     act = agent.act(p)
     env.update(act)
+    print('Ação realizada: ' + act)
+    
+    p = agent.perceives(env)
+    sala = 'A' if p.getLocation() else 'B'
+    estado = 'suja' if p.getIsDirty() else 'limpa'
+
+    print('Situação após a ação: Sala '+sala+' estado '+estado)
+
+
+   
